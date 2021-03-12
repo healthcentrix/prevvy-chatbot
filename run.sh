e46f8d41264849f6df80294c124249e6cfdfbc14
Roybert Parada <rparada@healthcentrix.com>
2021-03-12 01:07:23 -0400

    PB-281 Fix gcloud secret manager command

--- run.sh
+++ run.sh
@@ -6,8 +6,7 @@ export  TWILIO_ACCOUNT_SID=$(gcloud secrets versions access latest \
    --secret="${PROJECT}-${NAMESPACE}-chatbot-twilio-account-sid")
 export  TWILIO_AUTH_TOKEN=$(gcloud secrets versions access latest \
    --secret="${PROJECT}-${NAMESPACE}-chatbot-twilio-auth-token")
-export  REDIS_PASS=$($GCLOUD secrets versions access latest \
+export  REDIS_PASS=$(gcloud secrets versions access latest \
    --secret="${PROJECT}-${NAMESPACE}-chatbot-twilio-redis-pass")
-
 cd /usr/src/app
 npm run start
