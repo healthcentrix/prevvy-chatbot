9cdd754bde048c795eed2d3624b3f737b50065e1
srrmendez <sebastiancuba1991@gmail.com>
2021-04-28 12:08:48 -0400

    refactor: change response

--- src/util/data/redis-helper.ts
+++ src/util/data/redis-helper.ts
@@ -68,14 +68,13 @@ export class RedisHelper {
     }
 
     public async removeKey(key: string): Promise<void> {
-        const removeKeyPromise = (): Promise<string> => {
+        const removeKeyPromise = (): Promise<number> => {
             return new Promise((resolve, reject) => {
                 this.client.del(key, (err, resp) => {
                     if (err) {
-                        console.log("Redis error");
-                        console.log(err);
                         return reject(err);
                     }
+                    resolve(resp);
                 });
             });
         };
